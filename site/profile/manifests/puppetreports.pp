24a96ae1d1c9dc4401053fabc94751bc6cac9215
byzent <byzent@hotmail.com>
2018-08-13 10:59:14 +0200

    Fixed puppetdb_port
    Changed puppetdb_port to an integer

--- site/profile/manifests/puppetreports.pp
+++ site/profile/manifests/puppetreports.pp
@@ -31,7 +31,7 @@ class profile::puppetreports(
     groups              => 'puppet',
     manage_virtualenv   => true,
     puppetdb_host       => hiera('puppetdb_host'),
-    puppetdb_port       => '8081',
+    puppetdb_port       => 8081,
     puppetdb_key        => "${ssl_dir}/private_keys/${puppetboard_certname}.pem",
     puppetdb_ssl_verify => false,
     puppetdb_cert       => "${ssl_dir}/certs/${puppetboard_certname}.pem",
